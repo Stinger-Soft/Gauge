03539df3b15a12b96d06f0fc9fd5852e248ab943
Florian Meyer <florian.meyer@stinger-soft.net>
2016-07-13 17:21:35 +0200

    Implemented loadNext slide function

--- src/StingerSoft/GaugePresentationBundle/Resources/public/scripts/presentation.js
+++ src/StingerSoft/GaugePresentationBundle/Resources/public/scripts/presentation.js
@@ -19,8 +19,8 @@ StingerSoft.Gauge.Backend.Presentation.currentSlide = false;
 StingerSoft.Gauge.Backend.Presentation.loadNext = function(){
 	var nextSlideIndex = StingerSoft.Gauge.Backend.Presentation.slides.find(StingerSoft.Gauge.Backend.Presentation.currentSlide);
 	if(nextSlideIndex > 0 && nextSlideIndex < StingerSoft.Gauge.Backend.Presentation.slides.length){
-		//var url = Routing.generate('', {'slide': nextSlideIndex});
-		//StingerSoft.Gauge.Backend.Presentation.loadSlide(url);
+		var url = Routing.generate('stinger_soft_gauge_presentation_slide', {'slide': nextSlideIndex});
+		StingerSoft.Gauge.Backend.Presentation.loadSlide(url);
 	}
 };
 
