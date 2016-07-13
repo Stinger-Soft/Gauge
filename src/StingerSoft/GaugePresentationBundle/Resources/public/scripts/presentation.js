7e33174fe2be9b75f1e44fcc6d02a8e577a8c33d
Florian Meyer <florian.meyer@stinger-soft.net>
2016-07-13 17:26:32 +0200

    Fixed StingerSoft.Gauge.Backend.Presentation.loadNext

--- src/StingerSoft/GaugePresentationBundle/Resources/public/scripts/presentation.js
+++ src/StingerSoft/GaugePresentationBundle/Resources/public/scripts/presentation.js
@@ -17,9 +17,10 @@ StingerSoft.Gauge.Backend.Presentation.slides = [];
 StingerSoft.Gauge.Backend.Presentation.currentSlide = false;
 
 StingerSoft.Gauge.Backend.Presentation.loadNext = function(){
-	var nextSlideIndex = StingerSoft.Gauge.Backend.Presentation.slides.find(StingerSoft.Gauge.Backend.Presentation.currentSlide);
+	var nextSlideIndex = StingerSoft.Gauge.Backend.Presentation.slides.indexOf(StingerSoft.Gauge.Backend.Presentation.currentSlide) + 1;
 	if(nextSlideIndex > 0 && nextSlideIndex < StingerSoft.Gauge.Backend.Presentation.slides.length){
-		var url = Routing.generate('stinger_soft_gauge_presentation_slide', {'slide': nextSlideIndex});
+		var nextSlide = StingerSoft.Gauge.Backend.Presentation.slides[nextSlideIndex];
+		var url = Routing.generate('stinger_soft_gauge_presentation_slide', {'slide': nextSlide});
 		StingerSoft.Gauge.Backend.Presentation.loadSlide(url);
 	}
 };
